03ca7e156eb6bfc30bdf5ad286729b83d01ef8e8
sseyeon <seyeon9809@gmail.com>
2023-11-09 14:09:50 +0900

    add Auth module

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,16 @@
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  salt: string;
+}
